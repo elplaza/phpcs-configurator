f28e0d64f744faf9fcf58ebaa7f2b9cd14d54319
Federico Piazzon <federico.piazzon@gmail.com>
2019-02-14 18:34:23 +0100

    list all sniffs with choice

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -9,6 +9,12 @@ use Symfony\Component\Console\Input\InputArgument;
 //use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use PHP_CodeSniffer\Config;
+use PHP_CodeSniffer\Ruleset;
+use PHP_CodeSniffer\Util\Standards;
+use PHP_CodeSniffer\Generators\Text;
+
+use Elplaza\TextGenerator;
 
 class InitCommand extends Command
 {
@@ -39,12 +45,12 @@ class InitCommand extends Command
 
 		$fullpath = $path . DIRECTORY_SEPARATOR . $name;
 
-		// check if already exists
+		// create coding standard directory
 		if (file_exists($fullpath)) {
-			$question = new ConfirmationQuestion(
-				"Directory $fullpath already exists, you want override (default is NOT)? ",
-				false
-			);
+			$message = "Directory $fullpath <comment>already exists</comment>,"
+				. " you want override (default is NOT)? "
+			;
+			$question = new ConfirmationQuestion($message, false);
 
 			if (!$helper->ask($input, $output, $question)) {
 				$output->writeln(
@@ -58,9 +64,8 @@ class InitCommand extends Command
 
 		mkdir($fullpath);
 
-		die(var_dump($fullpath));
-
-		$sniffs = array("AAA", "BBB", "CCC");
+		// create the ruleset.xml
+		$sniffs = $this->getAllSniffs();
 		if (!empty($sniffs)) {
 			$output->writeln(
 				sprintf(
@@ -70,19 +75,157 @@ class InitCommand extends Command
 			);
 
 			foreach ($sniffs as $sniff) {
-				$output->writeln("sniff: $sniff");
+				$generator = $sniff["generator"];
+
+				$output->writeln($generator->generate());
+
+				$question = new ConfirmationQuestion("Do you want to enable this sniff? ", false);
+
+				$sniff["choice"] = $helper->ask($input, $output, $question);
 			}
-//            $this->choiceSniffs($sniffs, $input, $output);
-//            $this->writeRuleset($input, $output);
+
+		//	$xml = new SimpleXMLElement($xmlstr);
+
+//            $this->writeRuleset($input, $output, $choices);
 		} else {
 			$output->writeln("<error>✗</error> No sniffs can be found.");
 		}
+
+//			$generator->generate();
+//		die(var_dump(count($sniffs)));
+
+		// add all coding standard needs
+
+		// create another command to create custom sniffs
+		// ask for custom sniff: if yes create directories and files
+
+	}
+
+	private function getAllSniffs()
+	{
+		$sniffs    = array();
+		$standards = Standards::getInstalledStandards(true);
+		foreach ($standards as $standard) {
+			$sniffs = array_merge($sniffs, $this->getStandardInfo($standard));
+		}
+
+		return $sniffs;
+	}
+
+	private function getGenerator($standard)
+	{
+		$config = new Config(array(), false);
+		$config->standards = array($standard);
+		$ruleset = new Ruleset($config);
+		$ruleset->name = $standard;
+
+		return new TextGenerator($ruleset);
+	}
+
+	private function getSniffCode($sniffFile)
+	{
+		$sniffPart = str_replace("Sniff.php", "", $sniffFile);
+		$exploded  = explode(DIRECTORY_SEPARATOR, $sniffPart);
+		$parts     = array_slice($exploded, -4);
+
+		unset($parts[1]);
+
+		return implode(".", $parts);
+	}
+
+	private function getSniffDoc($sniffFile)
+	{
+		$docFile = str_replace(
+			DIRECTORY_SEPARATOR . "Sniffs" . DIRECTORY_SEPARATOR,
+			DIRECTORY_SEPARATOR . "Docs" . DIRECTORY_SEPARATOR,
+			$sniffFile
+		);
+
+		$docFile = str_replace("Sniff.php", "Standard.xml", $docFile);
+		return (is_file($docFile)) ? $docFile : null;
+	}
+
+	private function sniffDirPath($standard)
+	{
+		$rulesetPath  = Standards::getInstalledStandardPath($standard);
+		$standardPath = str_replace("ruleset.xml", "", $rulesetPath);
+		return $standardPath . "Sniffs" . DIRECTORY_SEPARATOR;
+	}
+
+	private function getStandardInfo($standard)
+	{
+		$generator = $this->getGenerator($standard);
+		$directory = $this->sniffDirPath($standard);
+
+		$sniffs = array();
+
+		$rdi = new \RecursiveDirectoryIterator(
+			$directory,
+			\RecursiveDirectoryIterator::FOLLOW_SYMLINKS
+		);
+
+		$di  = new \RecursiveIteratorIterator($rdi, 0, \RecursiveIteratorIterator::CATCH_GET_CHILD);
+
+		$dirLen = strlen($directory);
+
+		foreach ($di as $file) {
+			$filename = $file->getFilename();
+
+			// Skip hidden files.
+			if (substr($filename, 0, 1) === ".") {
+				continue;
+			}
+
+			// We are only interested in PHP and sniff files.
+			$fileParts = explode(".", $filename);
+			if (array_pop($fileParts) !== "php") {
+				continue;
+			}
+
+			$basename = basename($filename, ".php");
+			if (substr($basename, -5) !== "Sniff") {
+				continue;
+			}
+
+			$path = $file->getPathname();
+
+			// Skip files in hidden directories within the Sniffs directory of this
+			// standard. We use the offset with strpos() to allow hidden directories
+			// before, valid example:
+			// /home/foo/.composer/vendor/squiz/custom_tool/MyStandard/Sniffs/...
+			if (strpos($path, DIRECTORY_SEPARATOR . ".", $dirLen) !== false) {
+				continue;
+			}
+
+			$code = $this->getSniffCode($path);
+			$doc  = $this->getSniffDoc($path);
+
+			$generator->setDocFiles(array($doc));
+
+			$sniffs[] = array(
+				"standard"  => $standard,
+				"code"      => $code,
+				"path"      => $path,
+				"doc"       => $doc,
+				"generator" => $generator
+			);
+		}
+
+		return $sniffs;
+	}
+
+	private function askSniff(
+		InputInterface $input,
+		OutputInterface $output,
+		$sniff,
+		$verbose = false
+	) {
 	}
 
 	private function questionName()
 	{
 		$message = sprintf(
-			"Please enter the NAME of your php coding standard (default is %s): ",
+			"Please enter the <info>NAME</info> of your php coding standard (default is %s): ",
 			self::PHPCS_DEFAULT_NAME
 		);
 
@@ -98,7 +241,9 @@ class InitCommand extends Command
 
 	private function questionPath()
 	{
-		$question = new Question("Please enter the ABSOLUTE PATH DESTINATION DIRECTORY: ");
+		$question = new Question(
+			"Please enter the <info>ABSOLUTE PATH DESTINATION DIRECTORY</info>: "
+		);
 		$question
 			->setNormalizer(
 				function ($value) {
---
+++ src/TextGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Elplaza;
+
+use PHP_CodeSniffer\Ruleset;
+use PHP_CodeSniffer\Generators\Text;
+
+class TextGenerator extends Text
+{
+
+	public function __construct(Ruleset $ruleset)
+	{
+		$this->ruleset  = $ruleset;
+		$this->docFiles = array();
+	}
+
+	public function setDocFiles(array $files)
+	{
+		$this->docFiles = $files;
+		return $this;
+	}
+
+	public function processSniff(\DOMNode $doc)
+	{
+		if (PHPCSC_VERBOSITY === 0) {
+			$this->printTitle($doc);
+		} else {
+			foreach ($doc->childNodes as $node) {
+				if (PHPCSC_VERBOSITY === 1 && $node->nodeName === "standard") {
+					$this->printTextBlock($node);
+				} elseif (PHPCSC_VERBOSITY === 2 && $node->nodeName === "code_comparison") {
+					$this->printCodeComparisonBlock($node);
+				}
+			}
+		}
+	}
+
+}
